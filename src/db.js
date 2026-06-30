baa8664cb5e95e6431bdd5180998b6946157762f
Niyaz_G <niyazegg@gmail.com>
2026-06-30 12:14:50 +0500

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'matrix_db',
-  user: 'matrix',
-  password: 'matrix_pass',
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
